1f5d8500d23bed835749ae3edad63b4a1aa21fa9
yanglin <893713339@qq.com>
2020-06-20 23:29:31 +0800

    Eureka Server

---
+++ mall-parent/mall-eureka/src/main/java/com/mall/EurekaServerApplication.java
@@ -0,0 +1,23 @@
+package com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <p>
+ * Start up a Eureka Server
+ * </p>
+ *
+ * @author yanglin
+ * @date 2020-06-20 23:08:03
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class);
+    }
+
+}
