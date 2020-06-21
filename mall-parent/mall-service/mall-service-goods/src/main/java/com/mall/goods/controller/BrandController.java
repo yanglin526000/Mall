f6baf5f6df512ee44126c7b8dc356f11b96f1e33
yanglin <893713339@qq.com>
2020-06-21 18:05:23 +0800

    Custom MyBatisMapper

--- mall-parent/mall-service/mall-service-goods/src/main/java/com/mall/goods/controller/BrandController.java
+++ mall-parent/mall-service/mall-service-goods/src/main/java/com/mall/goods/controller/BrandController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-/****
- * @Author:admin
- * @Description:
- * @Date 2019/6/14 0:18
- *****/
-
+/**
+ * <p>
+ * Brand Controller
+ * </p>
+ *
+ * @author yanglin
+ * @date 2020-06-21 17:58:25
+ */
 @RestController
 @RequestMapping("/brand")
 @CrossOrigin
@@ -42,74 +44,5 @@ public class BrandController {
         List<Brand> result = brandService.findAll();
         return ResponseEntity.ok(result);
     }
-//
-//    @GetMapping(value = "/search/{page}/{size}")
-//    public Result<PageInfo> findPage(@PathVariable int page, @PathVariable int size) {
-//        //调用BrandService实现分页查询Brand
-//        PageInfo<Brand> pageInfo = brandService.findPage(page, size);
-//        return new Result<PageInfo>(true, StatusCode.OK, "查询成功", pageInfo);
-//    }
-//
-//    public Result<List<Brand>> findList(@RequestBody(required = false) Brand brand) {
-//        //调用BrandService实现条件查询Brand
-//        List<Brand> list = brandService.findList(brand);
-//        return new Result<List<Brand>>(true, StatusCode.OK, "查询成功", list);
-//    }
 
-//    @DeleteMapping(value = "/{id}")
-//    public Result delete(@PathVariable Integer id) {
-//        //调用BrandService实现根据主键删除
-//        brandService.delete(id);
-//        return new Result(true, StatusCode.OK, "删除成功");
-//    }
-//
-//
-//    @PutMapping(value = "/{id}")
-//    public Result update(@RequestBody Brand brand, @PathVariable Integer id) {
-//        //设置主键值
-//        brand.setId(id);
-//        //调用BrandService实现修改Brand
-//        brandService.update(brand);
-//        return new Result(true, StatusCode.OK, "修改成功");
-//    }
-//
-//
-//    @PostMapping
-//    public Result add(@RequestBody Brand brand) {
-//        //调用BrandService实现添加Brand
-//        brandService.add(brand);
-//        return new Result(true, StatusCode.OK, "添加成功");
-//    }
-//
-//
-//    @GetMapping("/{id}")
-//    public Result<Brand> findById(@PathVariable Integer id) {
-//        //调用BrandService实现根据主键查询Brand
-//        Brand brand = brandService.findById(id);
-//        return new Result<Brand>(true, StatusCode.OK, "查询成功", brand);
-//    }
-//
-//
-//    @GetMapping
-//    public Result<List<Brand>> findAll() {
-//        try {
-//            System.out.println("aaaaa=====");
-//            Thread.sleep(3000);
-//            System.out.println("bbbb=====");
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        //调用BrandService实现查询所有Brand
-//        List<Brand> list = brandService.findAll();
-//        return new Result<List<Brand>>(true, StatusCode.OK, "查询成功", list);
-//    }
-//
-//
-//    @GetMapping("/category/{id}")
-//    public Result<List<Brand>> findBrandByCategory(@PathVariable(name = "id") Integer id) {
-//        List<Brand> brandList = brandService.findByCategory(id);
-//
-//        return new Result<List<Brand>>(true, StatusCode.OK, "查询品牌列表成功", brandList);
-//
-//    }
 }
--- mall-parent/mall-service/mall-service-goods/src/main/java/com/mall/goods/dao/BrandMapper.java
+++ mall-parent/mall-service/mall-service-goods/src/main/java/com/mall/goods/dao/BrandMapper.java
@@ -1,8 +1,9 @@
 package com.mall.goods.dao;
 
+import com.mall.goods.mapper.MyBatisMapper;
 import com.mall.pojo.Brand;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
@@ -14,8 +15,8 @@ import java.util.List;
  * @author yanglin
  * @date 2020-06-21 15:22:22
  */
-@org.apache.ibatis.annotations.Mapper
-public interface BrandMapper extends Mapper<Brand> {
+@Mapper
+public interface BrandMapper extends MyBatisMapper<Brand> {
 
     @Select(value = "select tb.* from tb_brand tb ,tb_category_brand tbc where tb.id = tbc.brand_id and tbc.category_id=#{categoryid}")
     List<Brand> findByCategory(Integer categoryid);
