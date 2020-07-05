2bb3ea700d8ce60b138754e879a36e13dc8940b0
yanglin <893713339@qq.com>
2020-07-06 00:01:46 +0800

    解决了在@Configuration上使用了@EnableMvc以后，导致静态资源路径无法访问，Swagger2不能访问

--- mall-parent/mall-common/src/main/java/com/mall/common/base/config/CORSConfig.java
+++ mall-parent/mall-common/src/main/java/com/mall/common/base/config/CORSConfig.java
@@ -3,6 +3,7 @@ package com.mall.common.base.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -29,5 +30,25 @@ public class CORSConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 // 跨域允许时间
                 .maxAge(3600);
+    }
+
+    /**
+     * <p>
+     * 使用了@EnableMvc以后，导致静态资源路径无法访问
+     * 重新配置加上静态资源路径的映射
+     * </p>
+     *
+     * @param registry
+     * @author yanglin
+     * @date 2020-07-05 23:59:56
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 解决静态资源无法访问
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        // 解决swagger无法访问
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        // 解决swagger的js文件无法访问
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
