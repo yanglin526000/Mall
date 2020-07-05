4cce0379bfb6b943c0b4030bc2830846265ecd2a
yanglin <893713339@qq.com>
2020-07-05 23:49:48 +0800

    允许全局跨域配置

---
+++ mall-parent/mall-common/src/main/java/com/mall/common/base/config/CORSConfig.java
@@ -0,0 +1,33 @@
+package com.mall.common.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * <p>
+ * 允许全局跨域配置
+ * </p>
+ *
+ * @author yanglin
+ * @date 2020-07-05 23:47:57
+ */
+@Configuration
+@EnableWebMvc
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOrigins("*")
+                // 是否允许证书 不再默认开启
+                .allowCredentials(true)
+                // 设置允许的方法
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+}
