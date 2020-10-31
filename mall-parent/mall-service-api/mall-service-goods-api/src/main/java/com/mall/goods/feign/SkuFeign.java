d8c270e3b8676864ad47a8c7a5182eff145702d4
yanglin <893713339@qq.com>
2020-10-31 22:36:51 +0800

    Sku的常用Feign调用方式

--- mall-parent/mall-service-api/mall-service-goods-api/src/main/java/com/mall/goods/feign/SkuFeign.java
+++ mall-parent/mall-service-api/mall-service-goods-api/src/main/java/com/mall/goods/feign/SkuFeign.java
@@ -4,10 +4,9 @@ import com.mall.common.utils.ConstantUtil;
 import com.mall.goods.pojo.Sku;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,8 +21,31 @@ import java.util.Map;
 @RequestMapping("/sku")
 public interface SkuFeign {
 
+    @PostMapping
+    ResponseEntity<Sku> add(@RequestBody Sku t);
+
+    @PutMapping("{id}")
+    ResponseEntity<Sku> update(@PathVariable String id, @RequestBody Sku t);
+
+    @DeleteMapping("{id}")
+    ResponseEntity<Sku> deleteById(@PathVariable String id);
+
+    @GetMapping("{id}")
+    ResponseEntity<Sku> getInfoById(@PathVariable String id);
+
+    @GetMapping("fuzzyQuery")
+    ResponseEntity<Map<String, Object>> fuzzyQuery(@RequestParam Sku t,
+                                                   @RequestParam(value = "page", required = false, defaultValue = ConstantUtil.DEFAULT_PAGE_INDEX) Integer page,
+                                                   @RequestParam(value = "size", required = false, defaultValue = ConstantUtil.DEFAULT_PAGE_SIZE) Integer size);
+
     @GetMapping("exactSearch")
-    public ResponseEntity<Map<String, Object>> exactSearch(@RequestParam Sku t,
-                                                           @RequestParam(value = "page", required = false, defaultValue = ConstantUtil.DEFAULT_PAGE_INDEX) Integer page,
-                                                           @RequestParam(value = "size", required = false, defaultValue = ConstantUtil.DEFAULT_PAGE_SIZE) Integer size);
+    ResponseEntity<Map<String, Object>> exactSearch(@RequestParam Sku t,
+                                                    @RequestParam(value = "page", required = false, defaultValue = ConstantUtil.DEFAULT_PAGE_INDEX) Integer page,
+                                                    @RequestParam(value = "size", required = false, defaultValue = ConstantUtil.DEFAULT_PAGE_SIZE) Integer size);
+
+    @PostMapping("batchAdd")
+    ResponseEntity<List<Sku>> batchAdd(@RequestBody List<Sku> t);
+
+    @DeleteMapping("deleteByCond")
+    ResponseEntity<List<Sku>> deleteByCond(@RequestBody Sku t);
 }
--- mall-parent/mall-service/mall-service-search/src/main/java/com/mall/search/controller/SkuController.java
+++ mall-parent/mall-service/mall-service-search/src/main/java/com/mall/search/controller/SkuController.java
@@ -2,13 +2,15 @@ package com.mall.search.controller;
 
 import com.mall.goods.feign.SkuFeign;
 import com.mall.goods.pojo.Sku;
+import com.mall.search.pojo.SkuInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -21,16 +23,19 @@ import java.util.Map;
  * @date 2020-10-31 17:21:09
  */
 @RestController
-@CrossOrigin
 @RequestMapping("/search")
 public class SkuController {
 
     @Autowired
     private SkuFeign skuFeign;
 
+    @Autowired
+    private DiscoveryClient client;
+
     @GetMapping("/import")
     public ResponseEntity<Map<String, Object>> importEs() {
         ResponseEntity<Map<String, Object>> r = skuFeign.exactSearch(new Sku(), 0, 100);
+        List<SkuInfo> skuInfoList = (List<SkuInfo>) r.getBody().get("data");
         return r;
     }
 
