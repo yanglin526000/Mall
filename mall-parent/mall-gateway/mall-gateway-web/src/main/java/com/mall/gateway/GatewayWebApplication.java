8b26135d1aee5721f1c15a40be805917e9480c9b
yanglin <893713339@qq.com>
2020-11-14 00:02:51 +0800

    上传Gateway的测试成功的配置文件 添加配置参数的注释 基于令牌桶算法，根据IP设置限流，设置每秒请求数以及每秒并发数，并测试成功

--- mall-parent/mall-gateway/mall-gateway-web/src/main/java/com/mall/gateway/GatewayWebApplication.java
+++ mall-parent/mall-gateway/mall-gateway-web/src/main/java/com/mall/gateway/GatewayWebApplication.java
@@ -2,7 +2,12 @@ package com.mall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 /**
  * <p>
@@ -20,4 +25,27 @@ public class GatewayWebApplication {
         SpringApplication.run(GatewayWebApplication.class, args);
     }
 
+    /**
+     * <p>
+     * ip 限流操作
+     * </p>
+     *
+     * @return org.springframework.cloud.gateway.filter.ratelimit.KeyResolver
+     * @author yanglin
+     * @date 2020-11-13 23:39:44
+     */
+    @Bean(name = "ipKeyResolver")
+    public KeyResolver keyResolver() {
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                //1.获取请求request对象
+                ServerHttpRequest request = exchange.getRequest();
+                //2.从request中获取ip地址
+                String hostString = request.getRemoteAddress().getHostString();//Ip地址
+                //3.返回
+                return Mono.just(hostString);
+            }
+        };
+    }
 }
